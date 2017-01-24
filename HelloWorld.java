c2e1e118418f195b9b93d1dd911a23eb3bf6dedb
Sami Rollins <srollins@cs.usfca.edu>
2017-01-24 11:29:29 -0800

    update helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,11 @@
 	A program that says hello.
 	@author Sami Rollins
 **/
+
 public class HelloWorld {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, CS 112!");
 	}
 
 }
