3bed328e744cbec1be4662821a9a42343683f460
Sami Rollins <srollins@cs.usfca.edu>
2017-02-13 14:33:40 -0800

    add student example

---
+++ StudentList.java
@@ -0,0 +1,44 @@
+/**
+	A class to hold information about a list of
+	Students.
+	@author Sami Rollins
+**/
+
+import java.util.ArrayList;
+
+public class StudentList {
+
+	//data member: list to hold Student objects
+
+	//constructor
+	public StudentList() {
+
+	}	
+
+	//method to add a student to the list
+	public void addStudent(Student stuToAdd) {
+
+	}
+
+	//method to return a String representation of entire list
+	public String toString() {
+
+	}
+
+	//method to calculate average score across all students
+	public double calculateAverage() {
+
+		//method implementation replaced by algorithm
+
+		//declare variable to hold total
+
+		//for each Student in list
+		// add student score to total
+
+		//divide total by number of students
+		
+		//return result
+		return 0; //modify to implement correct logic
+	}
+
+}
